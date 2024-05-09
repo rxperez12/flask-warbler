afca61899851d669970593f7ebc0e585871a1ab1
Jacob <jacobrashid404@gmail.com>
2024-05-09 16:22:08 -0700

    implemented tests for deleting message and liking your own message

--- test_message_views.py
+++ test_message_views.py
@@ -4,7 +4,7 @@ import os
 from unittest import TestCase
 
 from app import app, CURR_USER_KEY
-from models import db, dbx, Message, User
+from models import db, dbx, Message, User, Like, Follow
 
 # To run the tests, you must provide a "test database", since these tests
 # delete & recreate the tables & data. In your shell:
@@ -37,13 +37,22 @@ class MessageBaseViewTestCase(TestCase):
 
         u1 = User.signup("u1", "u1@email.com", "password", None)
         db.session.flush()
+        
+        u2 = User.signup("u2", "u2@email.com", "password", None)
+        db.session.flush()
 
         m1 = Message(text="m1-text", user_id=u1.id)
-        db.session.add_all([m1])
+        m2 = Message(test="m2-text", user_id=u2.id)
+        
+        db.session.add_all([m1, m2])
         db.session.commit()
+        
 
         self.u1_id = u1.id
         self.m1_id = m1.id
+        
+        self.u2_id = u2.id
+        self.m2_id = m2.id
 
 
 class MessageAddViewTestCase(MessageBaseViewTestCase):
@@ -63,3 +72,55 @@ class MessageAddViewTestCase(MessageBaseViewTestCase):
             q = db.select(Message).filter_by(text="Hello")
             message = dbx(q).scalar_one_or_none()
             self.assertIsNotNone(message)
+            
+class MessageShowViewTestCase(MessageBaseViewTestCase):
+    def test_show_message(self):
+        # mimic logging in
+        with app.test_client() as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1_id
+        
+        resp = c.get(f"/messages/{self.m1_id}")
+        html = resp.get_data(as_text=True)
+        
+        self.assertEqual(resp.status_code, 200)
+        
+        self.assertIn("<!-- comment for testing message show -->", html)
+                
+class MessageDeleteViewTestCase(MessageBaseViewTestCase):
+    def test_delete_message(self):
+        # mimic logging in
+        with app.test_client() as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1_id
+                
+        q_msg = db.select(Message)
+        messages = dbx(q_msg).all()
+        self.assertEqual(len(messages), 1)
+                
+        resp = c.post(f"/messages/{self.m1_id}/delete")
+        
+        self.assertEqual(resp.status_code, 302)
+        
+        q_msg_after_del = db.select(Message)
+        messages_after_del = dbx(q_msg_after_del).all()
+        self.assertEqual(len(messages_after_del), 0)
+        
+class MessageAddLikeViewTestCase(MessageBaseViewTestCase):
+    def test_liking_own_message(self):
+        # mimic logging in
+        with app.test_client() as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1_id
+        
+        q_like = db.select(Like)
+        likes = dbx(q_like).all()
+        self.assertEqual(len(likes), 0)
+        
+        resp = c.post(f"/messages/{self.m1_id}/like")
+        
+        self.assertEqual(resp.status_code, 302)
+        
+        q_likes_after_add_like = db.select(Like)
+        likes_after_add_like = dbx(q_likes_after_add_like).all()
+        self.assertEqual(len(likes_after_add_like), 0)
